64ddaedd0e464df7b261b453cf40e680b7c0f994
Sumitkhadka10 <np03cs4a220361@heraldcollege.edu.np>
2024-03-31 07:55:34 +0545

    Project

---
+++ react-car-rental-website-master/src/UI/FindCarForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/find-car-form.css';
+
+import { Form, FormGroup } from 'reactstrap';
+
+
+const FindCarForm = () => {
+  return (
+    <Form>
+      <div className='d-flex align-items-center justify-content-between flex-wrap'>
+        <FormGroup className='form__group'>
+
+        </FormGroup>
+      </div>
+    </Form>
+  )
+}
+
+export default FindCarForm
